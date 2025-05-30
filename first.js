99da59d9038ca22483b1f2f4bdfe1b4c473915fa
Namanjain026 <namanjain9404@gmail.com>
2025-05-30 19:56:12 +0530

    add html,css and js portfolio files

---
+++ first.js
@@ -0,0 +1,10 @@
+var typed = new Typed("#element", {
+  strings: [
+    "MERN Stack Developer",
+    "Game Developer",
+    "Graphics Designer",
+    // "MERN Stack Developer",
+  ],
+  typeSpeed: 100,
+  loop: true,
+});
